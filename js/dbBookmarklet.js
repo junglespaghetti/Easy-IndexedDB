e44c39ffd2a73bd971303642fd29096ecd14516c
Glitch (easy-indexeddb) <none>
2020-03-25 05:25:26 +0000

    🌘🍣 Updated with Glitch

---
+++ js/dbBookmarklet.js
@@ -0,0 +1,9 @@
+function test(){
+  let div = document.createElement("div");
+  div.innerHTML = '<div id="table"></vid>';
+  div.style.display = "none"
+  document.body.appendChild(div);
+  
+  
+  
+}
