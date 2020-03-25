f80989b3191a798fc1fea3099b688d30a1c9b000
Glitch (easy-indexeddb) <none>
2020-03-25 06:49:22 +0000

    🚠📠 Updated with Glitch

--- dbBookmarklet.js
+++ dbBookmarklet.js
@@ -1,11 +1,13 @@
 javascript: (function(f, urls, cssUlrs, i, s, c) {
   urls = [
     "https://cdnjs.cloudflare.com/ajax/libs/alasql/0.5.5/alasql.min.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/Ventus/0.3.0/ventus.min.js",
+    "https://cdn.jsdelivr.net/npm/jspanel4@4.9.4/dist/jspanel.js",
     "https://cdnjs.cloudflare.com/ajax/libs/handsontable/7.4.2/handsontable.full.min.js",
     "https://easy-indexeddb.glitch.me/js/dbBookmarklet.js"
   ];
   cssUlrs = [
+    "https://use.fontawesome.com/releases/v5.6.3/css/all.css",
+    "https://cdn.jsdelivr.net/npm/jspanel4@4.9.4/dist/jspanel.css",
     "https://junglespaghetti.github.io/trade-recorder/css/trade-recoder.css"
   ];
   for (i = 0; i < cssUlrs.length; i++) {
