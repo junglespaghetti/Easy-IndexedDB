504f854a56f0499f7840d9c171f587ac0384189b
Glitch (easy-indexeddb) <none>
2020-03-28 18:56:31 +0000

    💐🙏 Updated with Glitch

--- dbBookmarklet.js
+++ dbBookmarklet.js
@@ -1,36 +1,12 @@
-javascript: (function(f, urls, cssUlrs, i, s, c) {
-   gsUrl =
+javascript: (function(s) {
+  gsUrl =
     "https://script.google.com/macros/s/AKfycbxKmy8wnZSKN2i-VlcVwcUutTu_4bvHdZ7zU7yvbCN0PFyONN4/exec";
-  scriptFanc = { pass: "test", callback: "jsonp_callback", origin:location.hostname };
-  urls = [
-    "https://cdnjs.cloudflare.com/ajax/libs/alasql/0.5.5/alasql.min.js",
-    "https://cdn.jsdelivr.net/npm/jspanel4@4.9.4/dist/jspanel.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/handsontable/7.4.2/handsontable.full.min.js",
-    "https://easy-indexeddb.glitch.me/js/dbBookmarklet.js"
-  ];
-  cssUlrs = [
-    "https://cdnjs.cloudflare.com/ajax/libs/handsontable/7.4.2/handsontable.full.min.css",
-    "https://use.fontawesome.com/releases/v5.6.3/css/all.css",
-    "https://cdn.jsdelivr.net/npm/jspanel4@4.9.4/dist/jspanel.css",
-    "https://junglespaghetti.github.io/trade-recorder/css/trade-recoder.css"
-  ];
-  for (i = 0; i < cssUlrs.length; i++) {
-    c = document.createElement("link");
-    c.type = "text/css";
-    c.rel = "stylesheet";
-    c.href = cssUlrs[i];
-    document.body.appendChild(c);
-  }
-  for (i = 0; i < urls.length; i++) {
-    s = document.createElement("script");
-    s.src = urls[i];
-    if (i == urls.length - 1) {
-      s.onload = function() {
-        f();
-      };
-    }
-    document.body.appendChild(s);
-  }
-})(function() {
-  loadJsonp(gsUrl, scriptFanc);
-});
+  scriptFanc = {
+    pass: "test",
+    callback: "jsonp_callback",
+    origin: location.hostname
+  };
+  s = document.createElement("script");
+  s.src = gsUrl + "?pass=" + scriptFanc.pass + "&loader=true";
+  document.body.appendChild(s);
+})();
